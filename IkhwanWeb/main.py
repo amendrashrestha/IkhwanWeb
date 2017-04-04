e31f1d032fedf5a7269f7d9028725e7c337ccd34
amendrashrestha <amendrashrestha@gmail.com>
2017-04-04 15:52:22 +0200

    Added folder

---
+++ IkhwanWeb/main.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+import os
+import traceback
+import time
+
+class IkhwanWebCrawler(object):
+    topic_url = "http://www.ikhwanweb.com/article.php?id=32799"
+
+    def __init__(self):
+        self.client = webdriver.Firefox()
+        self.extract_articles(self.topic_url)
+
+    def extract_articles(self, url):
+        try:
+            self.client.get(url)
+            time.sleep(10)
+            soup = bs(self.client.page_source, "lxml")
+
+            title = soup.title.string
+
+            container = soup.find("div",{"id":"ja-container"})
+            article = container.find("div",{"id":"vozme"})
+
+            print(title)
+            print(article.text)
+
+        except Exception:
+            traceback.print_exc()
+        self.client.quit()
+
+IkhwanWebCrawler()
+
+
+
+
+
+
+
